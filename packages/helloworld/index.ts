f6fc188df83daa53b3e9c280fd5d509239f4c3af
Manit Kapoor <manit.kapoor@swiggy.in>
2020-12-13 23:12:51 +0530

    added changed

--- packages/helloworld/index.ts
+++ packages/helloworld/index.ts
@@ -1,5 +1,9 @@
 const world = 'world';
 
+function testAnotherFunction(): string {
+    return "testAnotherFunction"
+}
+
 export function hello(word: string = world): string {
-  return `Hello test ${world}! `;
+  return `Hello test ${world}! ${testAnotherFunction()}`;
 }
